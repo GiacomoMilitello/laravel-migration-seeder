67afccc90534cc660173bcd6ad9d71b9c4376945
GiacomoMilitello <g.militello95@gmail.com>
2024-03-18 20:16:05 +0100

    controllers

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+
+        $trains = Train::where('departure_time', '>=', now())->get();
+
+        return view('pages.welcome', compact('trains'));
+    }
 }
